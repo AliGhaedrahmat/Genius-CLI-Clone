c0c0d1cd3bc298bed6eea0b1e39747d143f45397
Ali Ghaedrahmat <alighaedrahmat005@gmail.com>
2025-04-16 23:16:15 +0330

    Update build.gradle.kts

--- Genius_Java/build.gradle.kts
+++ Genius_Java/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    application
 }
 
 group = "org.example"
@@ -12,9 +13,25 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation ("com.google.code.gson:gson:2.8.9")
+    implementation("com.google.code.gson:gson:2.8.9")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+application {
+    mainClass.set("org.Main") // Change to your actual main class
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "org.Main" // Same here
+    }
+
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
 }
